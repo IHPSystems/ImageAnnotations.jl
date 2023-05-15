e0ca030e10190290148c55b5a83d13c04721d2ec
Jesper Stemann Andersen <jsa@ihp.dk>
2023-05-15 16:26:06 +0200

    Constrained image type for annotate to being an AbstractArray{<:Colorant, 2}

--- src/static_image_annotator.jl
+++ src/static_image_annotator.jl
@@ -6,6 +6,6 @@ struct StaticObjectAnnotator{L, T <: Real, A <: AbstractObjectAnnotation{L, T}} 
     annotations::Vector{A}
 end
 
-function annotate(image, annotator::Union{StaticImageAnnotator, StaticObjectAnnotator})
+function annotate(image::AbstractArray{<:Colorant,2}, annotator::Union{StaticImageAnnotator, StaticObjectAnnotator})
     return annotator.annotations
 end
